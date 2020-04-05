e65a783960ac146997a37aa6f852e109816eacc4
Sabari Giri <sabari.giri@quipserve.in>
2020-04-05 21:19:56 +0530

    Updates

---
+++ precache-manifest.a5f1de7b447429d622f3778c534fa862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668b9117ff082af529ac58813d39c531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7067ec1467f2062fcb16",
+    "url": "/static/css/main.91235e67.chunk.css"
+  },
+  {
+    "revision": "80a91e943bea3ced7e8f",
+    "url": "/static/js/2.bbb60601.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbb60601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7067ec1467f2062fcb16",
+    "url": "/static/js/main.901713a6.chunk.js"
+  },
+  {
+    "revision": "b573db2344e500f41bed",
+    "url": "/static/js/runtime-main.32378b55.js"
+  }
+]);
--- src/config/helpers.jsx
+++
@@ -1,6 +0,0 @@
-export const numberWithCommas = function (numberData) {
-    if(!numberData){
-        return "";
-    }
-    return parseFloat(numberData).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
