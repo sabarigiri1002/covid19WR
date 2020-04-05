93fc3386a752085541161e8c1d58381006b56858
Sabari Giri <sabari.giri@quipserve.in>
2020-04-05 21:12:31 +0530

    Updates

---
+++ precache-manifest.4e98490b1a0e31506c97910fd97e4f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc66e7228a5d8488b8fea17873bc870",
+    "url": "/covid-19-status/index.html"
+  },
+  {
+    "revision": "3c4f95f9d4207de79e94",
+    "url": "/covid-19-status/static/css/main.91235e67.chunk.css"
+  },
+  {
+    "revision": "80a91e943bea3ced7e8f",
+    "url": "/covid-19-status/static/js/2.bbb60601.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-status/static/js/2.bbb60601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4f95f9d4207de79e94",
+    "url": "/covid-19-status/static/js/main.532951c7.chunk.js"
+  },
+  {
+    "revision": "6d5715580f620366a7ec",
+    "url": "/covid-19-status/static/js/runtime-main.bc1bd186.js"
+  }
+]);
